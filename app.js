c89b8ec2debecd7317d94b5c3700a3bb27c659e7
NyeishaJPace <nyeisha.j.pace@gmail.com>
2022-10-12 21:31:06 +0000

    updated axios

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var mongoose = require('mongoose')
-var axios = require('axios').default;
+var axios = require('axios');
 var app = express();
 const bodyParser = require('body-parser');
 
